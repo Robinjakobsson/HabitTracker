98396f090e60bc63eecfb9e40e7bc16e371cc921
robinjakobsson <3ggapp24.jakoro@folkuniversitetet.nu>
2025-04-29 13:40:37 +0200

    Added Modelcontext.save to all of the Crud operations cause i felt it didn't save without it.
    Created a function to Mark habit as complete.
    
    Created a function to check if a habit is completed Today
    
    Created a ProgressView that visually displays the amount of habits completed today
    
    Created a CardView for the list to the display Streak/Title/ CreatedAtDate
    
    Wrote a few comments

---
+++ HabitTracker/View/HabitCardView.swift
@@ -0,0 +1,18 @@
+//
+//  HabitCardView.swift
+//  HabitTracker
+//
+//  Created by Robin jakobsson on 2025-04-29.
+//
+
+import SwiftUI
+
+struct HabitCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HabitCardView()
+}
