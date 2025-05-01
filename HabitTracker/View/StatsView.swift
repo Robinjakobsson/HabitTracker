88daf45e1d3c93634acb6d6f88026d6d01d50fe8
robinjakobsson <3ggapp24.jakoro@folkuniversitetet.nu>
2025-05-01 23:57:40 +0200

    Created StatsView and created TODOS for tomorrow.

--- HabitTracker/View/StatsView.swift
+++ HabitTracker/View/StatsView.swift
@@ -7,9 +7,30 @@
 
 import SwiftUI
 
+//TODO Friday
+
+// Gör klart Statsview så man ser högsta streaken som man gjort
+// Gör om TabView så den syns i botten här med
+// Lägg till bakgrund
+
 struct StatsView: View {
     var body: some View {
-        Text("Hello")
+        GridLayout {
+            GridRow {
+                Text("Hello")
+                Image(systemName: "circle.fill")
+            }
+            .frame(width: 100, height: 100)
+            .padding()
+            .background(Color(.systemGray6))
+            
+            GridRow {
+                Text("Hello")
+            }
+            .frame(width: 100, height: 100)
+            .padding()
+            .background(Color(.systemGray6))
+        }
     }
 }
 
